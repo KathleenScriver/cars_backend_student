15719736f8b1f3675d8b4506577ddb903aa7a7ee
KathleenScriver <klescriver@gmail.com>
2020-03-16 14:01:29 -0600

    UPdates for lesson.

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -3,4 +3,16 @@ class CarsController < ApplicationController
     @cars = Car.all
     render json: @cars
   end
+
+  def create
+    byebug
+    car = Car.create(
+      make: params[:make],
+      model: params[:model],
+      year: params[:year],
+      rating: params[:rating]
+    )
+    # render json: car
+    redirect_to 'http://localhost:3001'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cars, only: [:index]
+  resources :cars, only: [:index, :create]
 end
